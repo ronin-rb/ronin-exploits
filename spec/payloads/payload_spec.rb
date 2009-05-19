6d64a3647ea160b8a7c85b002336a6fa431213aa
postmodern <postmodern.mod3@gmail.com>
2009-05-19 12:40:59 -0700

    Added specs for the new Payload -> Exploit design.

--- spec/payloads/payload_spec.rb
+++ spec/payloads/payload_spec.rb
@@ -6,6 +6,7 @@ require 'helpers/objects'
 describe Payloads::Payload do
   before(:each) do
     @payload = load_payload('test')
+    @exploit = load_exploit('example')
   end
 
   it "should require a name attribute" do
@@ -49,16 +50,6 @@ describe Payloads::Payload do
     ]
   end
 
-  it "should allow for the extending of Helper modules" do
-    @payload.instance_eval { helper :shell }.should == true
-  end
-
-  it "should raise an UnknownHelper when extending an unknown helper" do
-    lambda {
-      @payload.instance_eval { helper :obvious_not_there }
-    }.should raise_error(Payloads::UnknownHelper)
-  end
-
   it "should not have an Arch by default" do
     @payload.arch.should be_nil
   end
@@ -80,19 +71,69 @@ describe Payloads::Payload do
     @payload.os.version.should == '7.1'
   end
 
+  it "should allow for the extending of Helper modules" do
+    @payload.instance_eval { helper :shell }.should == true
+  end
+
+  it "should raise an UnknownHelper when extending an unknown helper" do
+    lambda {
+      @payload.instance_eval { helper :obvious_not_there }
+    }.should raise_error(Payloads::UnknownHelper)
+  end
+
+  it "should not have an exploit by default" do
+    @payload.exploit.should be_nil
+  end
+
   it "should have 'built' and 'unbiult' states" do
     @payload.should_not be_built
     @payload.build!
     @payload.should be_built
   end
 
-  it "should return the built payload when calling build" do
-    @payload.build!.should == 'code'
+  it "should store the built payload in the @payload instance variable" do
+    @payload.build!
+    @payload.payload.should == "code.func"
+  end
+
+  it "should return the built payload when calling build!" do
+    @payload.build!.should == "code.func"
+  end
+
+  it "should use parameters in the building of the payload" do
+    @payload.custom = 'hello'
+    @payload.build!.should == "code.hello"
+  end
+
+  it "should have 'deployed' and 'undeployed' states" do
+    @payload.should_not be_deployed
+    @payload.build!
+    @payload.deploy!
+    @payload.should be_deployed
   end
 
   it "should have a default deployer method" do
     @payload.deploy! do |payload|
       payload.should == @payload
+    end
+  end
+
+  it "should pass the built payload to the exploit when deploying" do
+    @payload.exploit = @exploit
+
+    @payload.build!
+    @payload.deploy! do |payload|
+      payload.exploit.payload.should == "code.func"
+    end
+  end
+
+  it "should call the exploit when deployed" do
+    @payload.exploit = @exploit
+
+    @payload.build!
+    @payload.deploy! do |payload|
+      payload.exploit.should be_built
+      payload.exploit.should be_deployed
     end
   end
 end
