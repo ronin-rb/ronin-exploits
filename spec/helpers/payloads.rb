8cc23ef9fc0c163d3d66a6a8bec10949d74b7dc7
postmodern <postmodern.mod3@gmail.com>
2009-10-26 03:52:24 -0700

    Renamed spec/objects to spec/helpers/cache.

--- spec/helpers/payloads.rb
+++ spec/helpers/payloads.rb
@@ -1,7 +1,7 @@
 require 'ronin/payloads/payload'
 
 module Helpers
-  PAYLOADS_DIR = File.expand_path(File.join(File.dirname(__FILE__),'..','objects','payloads'))
+  PAYLOADS_DIR = File.expand_path(File.join(File.dirname(__FILE__),'cache','payloads'))
 
   def load_payload(name,base=Ronin::Payloads::Payload)
     base.load_from(File.join(PAYLOADS_DIR,"#{name}.rb"))
