fddb84e8dce118fd22c97e23211cdcb0acfe0479
postmodern <postmodern.mod3@gmail.com>
2009-10-26 03:49:28 -0700

    Fixed a require in 'ronin/payloads/shellcode'.

--- lib/ronin/payloads/shellcode.rb
+++ lib/ronin/payloads/shellcode.rb
@@ -19,7 +19,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
-require 'ronin/payloads/binary_payload'
+require 'ronin/payloads/asm_payload'
 
 module Ronin
   module Payloads
