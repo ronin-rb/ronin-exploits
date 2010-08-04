714a3c710b96369e16d8edba1a9faae205979085
Postmodern <postmodern.mod3@gmail.com>
2010-08-04 12:16:52 -0700

    No longer call Database.upgrade.

--- lib/ronin/payloads.rb
+++ lib/ronin/payloads.rb
@@ -19,14 +19,10 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
+require 'ronin/payloads/encoders'
+require 'ronin/payloads/payload'
+require 'ronin/payloads/binary_payload'
+require 'ronin/payloads/asm_payload'
+require 'ronin/payloads/nops'
+require 'ronin/payloads/shellcode'
 require 'ronin/payloads/payloads'
-require 'ronin/database'
-
-Ronin::Database.upgrade do
-  require 'ronin/payloads/encoders'
-  require 'ronin/payloads/payload'
-  require 'ronin/payloads/binary_payload'
-  require 'ronin/payloads/asm_payload'
-  require 'ronin/payloads/nops'
-  require 'ronin/payloads/shellcode'
-end
