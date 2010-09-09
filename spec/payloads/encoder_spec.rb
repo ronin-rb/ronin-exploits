49100b508c38225a57a6d238670c30b64419ba56
Postmodern <postmodern.mod3@gmail.com>
2010-09-09 04:27:15 -0700

    spec against Payloads::Encoder.

--- spec/payloads/encoder_spec.rb
+++ spec/payloads/encoder_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
-require 'ronin/payloads/encoders/encoder'
+require 'ronin/payloads/encoder'
 
-describe Payloads::Encoders::Encoder do
+describe Payloads::Encoder do
   before(:all) do
-    @encoder = Payloads::Encoders::Encoder.new(:name => 'test')
+    @encoder = Payloads::Encoder.new(:name => 'test')
     @data = 'some data'
   end
 
   it "should require a name" do
-    encoder = Payloads::Encoders::Encoder.new
+    encoder = Payloads::Encoder.new
     encoder.should_not be_valid
 
-    encoder = Payloads::Encoders::Encoder.new(:name => 'encoder')
+    encoder = Payloads::Encoder.new(:name => 'encoder')
     encoder.should be_valid
   end
 
@@ -24,6 +24,6 @@ describe Payloads::Encoders::Encoder do
   end
 
   it "should have a custom inspect method" do
-    @encoder.inspect.should == '#<Ronin::Payloads::Encoders::Encoder: test>'
+    @encoder.inspect.should == '#<Ronin::Payloads::Encoder: test>'
   end
 end
