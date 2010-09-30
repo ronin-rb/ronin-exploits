c52693b5b58d6abf476f3cccb2245394526df7b6
Postmodern <postmodern.mod3@gmail.com>
2010-09-29 22:46:36 -0700

    Use more 'let' in the specs.

--- spec/payloads/payload_spec.rb
+++ spec/payloads/payload_spec.rb
@@ -6,9 +6,7 @@ require 'helpers/payloads'
 describe Payloads::Payload do
   include Helpers
 
-  before(:each) do
-    @payload = load_payload('simple')
-  end
+  let(:payload) { load_payload('simple') }
 
   it "should require a name attribute" do
     payload = Payloads::Payload.new
@@ -39,44 +37,44 @@ describe Payloads::Payload do
   end
 
   it "should initialize all parameters by default" do
-    @payload.params.should_not be_empty
+    payload.params.should_not be_empty
   end
 
   it "should not have an Arch by default" do
-    @payload.arch.should be_nil
+    payload.arch.should be_nil
   end
 
   it "should not have an OS by default" do
-    @payload.os.should be_nil
+    payload.os.should be_nil
   end
 
   it "should allow for the extending of Helper modules" do
-    @payload.instance_eval { helper :bind_shell }.should == true
+    payload.instance_eval { helper :bind_shell }.should == true
   end
 
   it "should raise an UnknownHelper when extending an unknown helper" do
     lambda {
-      @payload.instance_eval { helper :obvious_not_there }
+      payload.instance_eval { helper :obvious_not_there }
     }.should raise_error(Payloads::UnknownHelper)
   end
 
   it "should not have an exploit by default" do
-    @payload.exploit.should be_nil
+    payload.exploit.should be_nil
   end
 
   it "should store the raw payload in the @raw_payload instance variable" do
-    @payload.build!
-    @payload.raw_payload.should == "code.func"
+    payload.build!
+    payload.raw_payload.should == "code.func"
   end
 
   it "should return the raw payload when calling build!" do
-    @payload.build!
-    @payload.raw_payload.should == "code.func"
+    payload.build!
+    payload.raw_payload.should == "code.func"
   end
 
   it "should use parameters in the building of the payload" do
-    @payload.custom = 'hello'
-    @payload.build!
-    @payload.raw_payload.should == "code.hello"
+    payload.custom = 'hello'
+    payload.build!
+    payload.raw_payload.should == "code.hello"
   end
 end
