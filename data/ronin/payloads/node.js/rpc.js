53a218d057bb32de9fcddb3b34e85d0ef69707c5
Postmodern <postmodern.mod3@gmail.com>
2011-12-26 16:48:53 -0800

    Refactored the node.js payload to have the same API as the Ruby one.

--- data/ronin/payloads/node.js/rpc.js
+++ data/ronin/payloads/node.js/rpc.js
@@ -1,243 +1,147 @@
-var Transports = {};
+var Util = require('util');
+var FS   = require('fs');
+var Process = require('child_process');
 
-/*
- * Creates a new Transport.
- */
-Transport = function() {}
+var RPC = {
+  /* fs functions */
+  fs_open: FS.openSync,
+  fs_read: function(fd,position,length) {
+    var buffer = new Buffer();
 
-/*
- * Starts the Transport and begins receiving Requests.
- *
- * callback - The function that will be passed the received Requests.
- */
-Transport.prototype.start = function(callback) {}
+    FS.readSync(fd,buffer,length);
+    return buffer;
+  },
+  fs_write: function(fd,position,data) {
+    var buffer = new Buffer(data);
 
-/*
- * Stops the Transport
- */
-Transport.prototype.stop = function() {}
+    return FS.writeSync(fd,buffer,0,buffer.length,position);
+  },
+  fs_close:  FS.closeSync,
+  fs_move:   FS.renameSync,
+  fs_unlink: FS.unlinkSync,
+  fs_rmdir:  FS.rmdirSync,
+  fs_mkdir:  FS.mkdirSync,
+  fs_chmod:  FS.chmodSync,
+  fs_stat:   FS.statSync,
+  fs_link:   FS.symlinkSync,
+
+  /* process functions */
+  process_getpid:    function() { return process.pid; },
+  process_getcwd: process.cwd,
+  process_chdir:  process.chdir,
+  process_getuid: process.getuid,
+  process_setuid: process.setuid,
+  process_getgid: process.getgid,
+  process_setgid: process.setgid,
+  process_getenv: function(name) { return process.env[name]; },
+  process_setenv: function(name,value) {
+    return process.env[name] = value;
+  },
+  process_unsetenv: function(name) {
+    var value = process.env[name];
+
+    delete process.env[name];
+    return value
+  },
+  process_time: function() { return new Date().getTime(); },
+  process_kill: process.kill,
+  process_exit: process.exit,
+
+  Shell: {
+    commands: {},
+
+    command: function(pid) {
+      var process = RPC.Shell.commands[pid];
+
+      if (process == undefined) {
+        throw("unknown command PID: " + pid);
+      }
+
+      return process;
+    }
+  },
+
+  shell_exec: function() {
+    process = Process.exec(arguments.join(' '));
+
+    RPC.Shell.commands[process.pid] = process;
+    return process.pid;
+  },
+  shell_read: function(pid,length) {
+    var process = RPC.Shell.command(pid);
+
+    process.stdin.resume();
+  },
+  shell_write: function(pid,data) {
+    var process = RPC.Shell.command(pid);
+
+    process.stdout.write(data);
+    return data.length;
+  },
+  shell_close: function(pid) {
+    var process = RPC.Shell.command(pid);
+
+    process.destroy();
+    delete RPC.Shell.commands[pid];
+    return true;
+  }
+};
+
+RPC.Transport = function() {}
+RPC.Transport.prototype.start = function() {}
+RPC.Transport.prototype.stop = function() {}
+
+RPC.Transport.prototype.lookup = function(name) {
+  return RPC[name];
+}
+
+RPC.Transport.prototype.dispatch = function(name,args) {
+  var func = this.lookup(name);
+
+  if (func == undefined) {
+    return this.error_message("unknown function: " + name);
+  }
+
+  try {
+    return this.return_message(func.apply(this,args));
+  } catch(error) {
+    return this.error_message(error.toString());
+  }
+}
+
+RPC.Transport.prototype.return_message = function(data) {
+  return {'return': data};
+}
+
+RPC.Transport.prototype.error_message = function(message) {
+  return {'exception': message};
+}
 
 /*
  * The default data serialization function.
  */
-Transport.prototype.serialize = function(data) {
+RPC.Transport.prototype.serialize = function(data) {
   return new Buffer(JSON.stringify(data)).toString('base64');
 }
 
 /*
  * The default data deserialization function.
  */
-Transport.prototype.deserialize = function(data) {
+RPC.Transport.prototype.deserialize = function(data) {
   return JSON.parse(new Buffer(data,'base64'));
-}
-
-/*
- * Sends data to the session.
- *
- * session - Abstract session belonging to the Transport.
- * data - The data to send.
- */
-Transport.prototype.send = function(session,data) {
-  session.write(this.serialize(data));
-}
-
-/*
- * transport - The transport that received the request.
- * session - Abstract session object.
- * name - Name of the function to call.
- * args - Additional arguments for the function.
- */
-Request = function(transport,session,name,args) {
-  this.transport = transport;
-  this.session   = session;
-
-  this.name = name;
-  this.args = args;
-}
-
-/*
- * Returns a callback used to yield data to the caller.
- */
-Request.prototype.callback = function() {
-  var self = this;
-
-  return function() {
-    self.transport.send(self.session, {'yield': arguments});
-  }
-}
-
-/*
-* Returns data for the request.
-*/
-Request.prototype.return = function(value) {
-  this.transport.send(this.session, {'return': value});
-}
-
-/*
- * Sends an Error in response to the request.
- *
- * message - The error message to send.
- */
-Request.prototype.error = function(message) {
-  this.transport.send(this.session, {'exception': message});
-}
-
-var Util = require('util');
-var FS   = require('fs');
-var Process = require('child_process');
-
-/*
- * Creates a new RPC object.
- *
- * transport - The Transport that will handle receiving and sending data.
- */
-RPC = function(transport) {
-  this.transport = transport;
-}
-
-/*
- * Wraps a function for RPC.functions.
- */
-RPC.wrap = function(func) {
-  return function(args,callback) { return func.apply(this,args); };
-}
-
-RPC.Shell = {
-  commands: {},
-
-  command: function(pid) {
-    var process = RPC.Shell.commands[pid];
-
-    if (process == undefined) {
-      throw("unknown command PID: " + pid);
-    }
-
-    return process;
-  }
-};
-
-/*
- * The RPC functions table.
- */
-RPC.functions = {
-  /* fs functions */
-  fs_open: RPC.wrap(FS.openSync),
-  fs_read: function(args) {
-    var buffer = new Buffer();
-
-    FS.readSync(args[0],buffer,args[1]);
-    return buffer;
-  },
-  fs_write: function(args) {
-    var buffer = new Buffer(args[2]);
-
-    return FS.writeSync(args[0],buffer,0,buffer.length,args[1]);
-  },
-  fs_close:  RPC.wrap(FS.closeSync),
-  fs_move:   RPC.wrap(FS.renameSync),
-  fs_unlink: RPC.wrap(FS.unlinkSync),
-  fs_rmdir:  RPC.wrap(FS.rmdirSync),
-  fs_mkdir:  RPC.wrap(FS.mkdirSync),
-  fs_chmod:  RPC.wrap(FS.chmodSync),
-  fs_stat:   RPC.wrap(FS.statSync),
-  fs_link:   RPC.wrap(FS.symlinkSync),
-
-  /* process functions */
-  process_pid:    function(args) { return process.pid; },
-  process_getcwd: RPC.wrap(process.cwd),
-  process_chdir:  RPC.wrap(process.chdir),
-  process_getuid: RPC.wrap(process.getuid),
-  process_setuid: RPC.wrap(process.setuid),
-  process_getgid: RPC.wrap(process.getgid),
-  process_setgid: RPC.wrap(process.setgid),
-  process_getenv: function(args) { return process.env[args[0]]; },
-  process_setenv: function(args) { return process.env[args[0]] = args[0]; },
-  process_unsetenv: function(args) {
-    var name  = args[0];
-    var value = process.env[name];
-
-    delete process.env[name];
-    return value
-  },
-  process_time:   function(args) { return new Date().getTime(); },
-  process_kill:   RPC.wrap(process.kill),
-  process_exit:   RPC.wrap(process.exit),
-
-  shell_exec: function(args,callback) {
-    process = Process.exec(args.join(' '));
-
-    RPC.Shell.commands[process.pid] = process;
-    return process.pid;
-  },
-  shell_read: function(args) {
-    var process = RPC.Shell.command(args[0]);
-
-    process.stdin.resume();
-  },
-  shell_write: function(args) {
-    var process = RPC.Shell.command(args[0]);
-
-    process.stdout.write(args[1]);
-    return args[1].length;
-  },
-  shell_close: function(args) {
-    var process = RPC.Shell.command(args[0]);
-
-    process.destroy();
-    delete RPC.Shell.commands[pid];
-    return true;
-  }
-}
-
-/*
- * Registers a function with the RPC.
- *
- * name - Name of the registered function.
- * func - The function that will receive the Requests.
- */
-RPC.prototype.registerFunction = function(name,func) {
-  RPC.functions[name] = func;
-}
-
-/*
- * Starts the Transport and begins processing requests.
- */
-RPC.prototype.start = function() {
-  var self = this;
-
-  this.transport.start(function(request) {
-    var func = RPC.functions[request.name];
-
-    if (func == undefined) {
-      request.error("Unknown function: " + request.name);
-    }
-
-    try {
-      request.return(func(request.args,request.callback()));
-    } catch(error) {
-      request.error(error.toString());
-    }
-  });
-}
-
-/*
- * Stops the Transport.
- */
-RPC.prototype.stop = function() {
-  this.transport.stop();
 }
 
 var HTTP = require('http');
 var URL  = require('url');
 
-Transports.HTTP = function(port,host) {
+RPC.HTTP = function(port,host) {
   this.port = port;
   this.host = (host ? host : '0.0.0.0');
 }
 
-Transports.HTTP.prototype = new Transport();
+RPC.HTTP.prototype = new RPC.Transport();
 
-Transports.HTTP.prototype.start = function(callback) {
+RPC.HTTP.prototype.start = function(callback) {
   var self = this;
 
   this.server = HTTP.createServer(function(request,response) {
@@ -245,8 +149,7 @@ Transports.HTTP.prototype.start = function(callback) {
     var name = url.pathname.slice(1,request.url.length).replace('/','_');
     var args = (url.query ? self.deserialize(url.query) : []);
 
-    callback(new Request(self,response,name,args));
-
+    response.write(self.serialize(self.dispatch(name,args)));
     response.end();
   });
 
@@ -255,11 +158,10 @@ Transports.HTTP.prototype.start = function(callback) {
   });
 }
 
-Transports.HTTP.prototype.stop = function() {
+RPC.HTTP.prototype.stop = function() {
   this.server.close();
 }
 
-var transport = new Transports.HTTP(process.argv[2],process.argv[3]);
-var rpc = new RPC(transport);
+var rpc = new RPC.HTTP(process.argv[2],process.argv[3]);
 
 rpc.start();
