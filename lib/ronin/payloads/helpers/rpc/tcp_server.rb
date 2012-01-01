9e665f4e33f2dd1094a931b815da01eda257b4a9
Postmodern <postmodern.mod3@gmail.com>
2011-12-31 19:48:48 -0800

    Added Payloads::Helpers::RPC::TCP for TCPServer and TCPConnectBack.

--- lib/ronin/payloads/helpers/rpc/tcp_server.rb
+++ lib/ronin/payloads/helpers/rpc/tcp_server.rb
@@ -20,11 +20,9 @@
 # along with Ronin.  If not, see <http://www.gnu.org/licenses/>
 #
 
+require 'ronin/payloads/helpers/rpc/tcp'
 require 'ronin/payloads/helpers/rpc/process'
 require 'ronin/network/mixins/tcp'
-
-require 'base64'
-require 'json'
 
 module Ronin
   module Payloads
@@ -50,6 +48,7 @@ module Ronin
         #     end
         #
         module TCPServer
+          include TCP
           include Process
 
           def self.extended(object)
@@ -69,70 +68,6 @@ module Ronin
               end
             end
           end
-
-          protected
-
-          #
-          # Encodes an RPC Request.
-          #
-          # @param [Hash] message
-          #   The message to send to the RPC Server.
-          #
-          # @return [String]
-          #   The encoded request.
-          #
-          # @api private
-          #
-          def rpc_encode_request(message)
-            Base64.encode64(message.to_json) + "\0"
-          end
-
-          #
-          # Decodes a RPC Response.
-          #
-          # @param [String] data
-          #   A message from the RPC Server.
-          #
-          # @return [Hash]
-          #   The decoded RPC response.
-          #
-          # @api private
-          #
-          def rpc_decode_response(data)
-            JSON.parse(Base64.decode64(data.chomp("\0")))
-          end
-
-          #
-          # Calls a function on the Server.
-          #
-          # @param [String, Symbol] name
-          #   The RPC function to call.
-          #
-          # @param [Array] arguments
-          #   Additional arguments to pass to the RPC function.
-          #
-          # @return [Object]
-          #   The return value from the RPC function.
-          #
-          # @raise [RuntimeError]
-          #   An exception raised by the RPC function.
-          #
-          # @api semipublic
-          #
-          def rpc_call(name,*arguments)
-            request = {:name => name, :arguments => arguments}
-
-            @connection.write(rpc_encode_request(request))
-
-            response = rpc_decode_response(@connection.readline("\0"))
-
-            if response['exception']
-              raise(response['exception'])
-            end
-
-            return response['return']
-          end
-
         end
       end
     end
