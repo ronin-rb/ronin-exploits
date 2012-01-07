47ebd704c78c9ab44beee4b491b2c3809075151f
Postmodern <postmodern.mod3@gmail.com>
2012-01-07 02:48:57 -0800

    Added the generic RPC Payload.

---
+++ lib/ronin/payloads/rpc.rb
@@ -0,0 +1,44 @@
+#
+# Ronin Exploits - A Ruby library for Ronin that provides exploitation and
+# payload crafting functionality.
+#
+# Copyright (c) 2007-2012 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of Ronin Exploits.
+#
+# Ronin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ronin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ronin.  If not, see <http://www.gnu.org/licenses/>
+#
+
+require 'ronin/payloads/payload'
+require 'ronin/payloads/helpers/rpc'
+
+module Ronin
+  module Payloads
+    class RPC < Payload
+
+      #
+      # Creates a new RPC payload.
+      #
+      # @param [Hash] attributes
+      #   Attributes for the payload.
+      #
+      def initialize(attributes={})
+        super(attributes)
+
+        helper :rpc
+      end
+
+    end
+  end
+end
