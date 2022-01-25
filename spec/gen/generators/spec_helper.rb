54dca7ea52b07845de9e2e0d083c1cca50797f36
Postmodern <postmodern.mod3@gmail.com>
2022-01-25 10:27:42 -0800

    Remove `ronin/payloads` and `ronin/encoders` in favor of ronin-payloads (closes #67).

--- spec/gen/generators/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'rspec'
-
-RSpec.configure do |spec|
-  spec.before(:suite) do
-    # disable the EDITOR for the Exploit / Payload generators.
-    ENV['EDITOR'] = nil
-  end
-end
--- spec/helpers/payloads.rb
+++
@@ -1,9 +0,0 @@
-require 'ronin/payloads/payload'
-
-module Helpers
-  PAYLOADS_DIR = File.expand_path(File.join(File.dirname(__FILE__),'scripts','payloads'))
-
-  def load_payload(name,base=Ronin::Payloads::Payload)
-    base.load_object(File.join(PAYLOADS_DIR,"#{name}.rb"))
-  end
-end
